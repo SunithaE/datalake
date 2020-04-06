7074ecf0359fe620a73a99b22144a268827a10ed
suneswar <suneswar@NOOSL2VS6YT2.crayon.as>
2020-04-06 16:19:59 +0200

    Added Kafka start stream

--- src/main/java/myapps/TestKafka.java
+++ src/main/java/myapps/TestKafka.java
@@ -1,8 +1,10 @@
 package myapps;
 
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
@@ -26,6 +28,30 @@ public class TestKafka {
 		
 		final Topology topology = builder.build();
 		System.out.println(topology.describe());
+		
+		
+		final KafkaStreams streams = new KafkaStreams(topology, props);
+		
+		final CountDownLatch latch = new CountDownLatch(1);
+		
+		Runtime.getRuntime().addShutdownHook(new Thread("stream-shutdown-hook") {
+			@Override
+			public void run() {
+				streams.close();
+				latch.countDown();
+			}
+		}
+				);
+		
+		try {
+			streams.start();
+			latch.await();
+		}
+		catch (Throwable e) {
+			// TODO: handle exception
+			System.exit(1);
+		}
+		System.exit(0);
 	}
 
 }
